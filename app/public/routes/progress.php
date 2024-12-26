2d99db53881ced0b7bd8689adfbca4835a9379ad
KimvanSchagen <95349001+KimvanSchagen@users.noreply.github.com>
2024-12-26 16:55:05 +0100

    progress page, phase 1

--- app/public/routes/progress.php
+++ app/public/routes/progress.php
@@ -1,9 +1,14 @@
 <?php
 
 require_once(__DIR__ . '/../lib/auth.php');
+require_once (__DIR__ . '/../lib/Leveling.php');
 
 Route::add('/progress', function () {
     if (isUser()) {
+        $levelingSystem = new LevelingSystem();
+
+        $user = $_SESSION['user'];
+        $pointsNeeded = $levelingSystem->getPointsNeeded($user['level']);
         require(__DIR__ . "/../views/pages/progress.php");
     } else {
         header("Location: /");
--- app/public/views/partials/progress_content.php
+++ app/public/views/partials/progress_content.php
@@ -1,1 +1,13 @@
 <h1>Progress</h1>
+<div class="progress-bar">
+    <progress value="<?php echo htmlspecialchars($user['current_points']) ?>" max="<?php echo $pointsNeeded ?>" class="achievementPoints"></progress>
+    <p><?php echo htmlspecialchars($user['current_points']) ?> / <?php echo $pointsNeeded ?></p>
+</div>
+
+<div>
+    <h3>Ongoing Quests</h3>
+</div>
+
+<div>
+    <h3>Completed Quests</h3>
+</div>
